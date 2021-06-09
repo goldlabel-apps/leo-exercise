71c08f63044439169addb560fb52d2920ede9727
listingslab <listingslab@gmail.com>
2021-06-09 21:28:48 +1000

    Creates the actual cart function

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -8,10 +8,14 @@ import {
 
 export interface CartState {
   items: any,
+  cart: any,
 }
 
 const initialState: CartState = {
   items: [],
+  cart: {
+    totalItems: 0,
+  },
 }
 
 export const cartSlice = createSlice({
@@ -20,10 +24,31 @@ export const cartSlice = createSlice({
   reducers: {
     addItem: (state, action: PayloadAction<string>) => {
       state.items = [...state.items, action.payload]
+      // Translate into useful object
+      const items = state.items
+      const cart = {
+        totalItems: 0,
+        totalPense: 0,
+        apples: 0,
+        oranges: 0,
+      }
+      for ( let i = 0; i < items.length; i ++){
+        const item = items[i]
+        if ( item === `apple` ) {
+          cart.apples += 1
+          cart.totalPense += 60
+        }
+        if ( item === `orange` ) {
+          cart.oranges += 1
+          cart.totalPense += 25
+        }
+        cart.totalItems += 1
+      }
+      state.cart = { ...state.cart, ...cart }
     }
   }
 })
 
 export const { addItem } = cartSlice.actions
-export const useItems = (state: RootState) => state.cart.items
+export const useCart = (state: RootState) => state.cart.cart
 export default cartSlice.reducer
